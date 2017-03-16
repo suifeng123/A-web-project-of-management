624a3066dc8e76767f6b900dd2742aeda570af1e
wangshengwen <wswen1220@163.com>
2017-03-16 10:10:56 +0800

    webmanagement

---
+++ testproject/test1/test2/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <Hello></Hello>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+import Hello from './Hello'
+
+export default {
+  components:{
+    Hello
+  }
+}
+</script>
---
+++ testproject/test1/test3/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App'
+import VueValidator from 'vue-validator'
+Vue.use(VueValidator)
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }
+})
