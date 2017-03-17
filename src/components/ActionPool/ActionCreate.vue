563127d633af41ffbc1a0f1a1849791ab22e8a98
wangshengwen <wswen1220@163.com>
2017-03-17 15:39:45 +0800

    webmanagement

--- src/components/ActionPool/ActionCreate.vue
+++ src/components/ActionPool/ActionCreate.vue
@@ -1,79 +1,84 @@
 <template>
   <div>
-  <MyHeader></MyHeader>
-  <Leftbar></Leftbar>
-  <div class="maindiv">
-    <div class="panel-head">创建存储池</div>
-    <novalidator name="validation">
-    <form novalidate>
-      <label for="haha">输入</label>
-     <input id="haha" type="text" v-validator:comment="{minlength:6,maxlength:10}"/>
-      <div>
-        <span v-show="$validation.comment.minlength">你的输入太短了</span>
-      </div>
-     <div class="form-group">
-     <label >名称*</label>
-     <input  type="text" class="col-lg" v-model="name"/>
-       </div>
-     <div class="form-group">
-     <label>控制器*</label>
-       <select required class="col-lg" v-model="controller">
-         <option v-for="option in controllers">
-           {{option}}
-         </option>
-       </select>
-     </div>
-     <div class="form-group">
-       <div class="div_table_left">
-       <label style="margin-top:-1000px;">成员盘</label>
-       </div>
-       <div style="display:inline-block;">
-         <table>
-         <thead>
-          <tr>
-            <td></td>
-            <td>名称</td>
-         </tr>
-         </thead>
-         <tbody>
-           <tr v-for="disk in disks" >
-           <td>
-             <label class="label1"><input type="checkbox" v-model="disk.checkbox" value="disk.checkbox" @click="changeDisk()"/><i></i> </label>
-           </td>
-             <td>{{disk.name}}</td>
-         </tr>
-         </tbody>
-       </table>
-         </div>
-     </div>
-     <div class="form-group">
-       <label>加密</label>
-       <input type="checkbox" v-model="entrycheck" />
-       </div>
-     <div class="form-group">
-       <label>重删</label>
-       <select required class="col-lg" v-model="initial">
-         <option v-for="option in options">
-         {{option}}
-         </option>
-       </select>
-     </div>
-     <div class="form-group">
-       <div class="div_table_left">
-       <label>其他</label>
-       </div>
-       <div style="float:left;">
-          <table>
-            <thead>
-               <tr>
-                 <td>磁盘</td>
-                 <td>无</td>
-                 <td>Log</td>
-                 <td>Cache</td>
-                 <td>Spare</td>
-               </tr>
-            </thead>
-            <tbody>
+    <MyHeader></MyHeader>
+    <Leftbar></Leftbar>
+    <div class="maindiv">
+      <vue-form :state="formstate" @submit.prevent="onSubmit">
+      <h1>创建存储池</h1>
+        <div class="form-group" style="display:block;">
+            <validate tag="label" class="validate">
+              <div style="display:inline-block;width:600px;">
+              <label>名称*</label>
+            <input style="display:inline-block;" type="text" class="col-lg" name="name" required v-model="name"/>
+              </div>
+                <div style="width:400px;" class="alert">
+              <form-error  field="name" error="required">这个字段是必须的</form-error>
+              </div>
+            </validate>
+          </div>
+          <div class="form-group">
+            <validate tag="label" class="validate">
+              <div style="display:inline-block;width:600px;">
+            <label>控制器*</label>
+            <select required class="col-lg" v-model="controller">
+              <option v-for="option in controllers">
+                {{option}}
+              </option>
+            </select>
+                </div>
+            </validate>
+          </div>
+          <div class="form-group">
+            <div class="div_table_left">
+              <label style="margin-top:-1000px;">成员盘</label>
+            </div>
+            <div style="display:inline-block;">
+              <table>
+                <thead>
+                <tr>
+                  <td></td>
+                  <td>名称</td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="disk in disks" >
+                  <td>
+                    <label class="label1"><input type="checkbox" v-model="disk.checkbox" value="disk.checkbox" @click="changeDisk()"/><i></i> </label>
+                  </td>
+                  <td>{{disk.name}}</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="form-group">
+            <label>加密</label>
+            <input type="checkbox" v-model="entrycheck" />
+          </div>
+          <div class="form-group">
+            <label>重删</label>
+            <select required class="col-lg" v-model="initial">
+              <option v-for="option in options">
+                {{option}}
+              </option>
+            </select>
+          </div>
+          <div class="form-group">
+            <div class="div_table_left">
+              <label>其他</label>
+            </div>
+            <div style="float:left;">
+              <table>
+                <thead>
+                <tr>
+                  <td>磁盘</td>
+                  <td>无</td>
+                  <td>Log</td>
+                  <td>Cache</td>
+                  <td>Spare</td>
+                </tr>
+                </thead>
+                <tbody>
                 <tr v-for="data in copydisks">
                   <td>{{data.name}}</td>
                   <td><input type="checkbox" v-model="check_none" value="check_none"/> </td>
@@ -81,35 +86,66 @@
                   <td><input type="checkbox"  /> </td>
                   <td><input type="checkbox"  /> </td>
                 </tr>
-            </tbody>
-          </table>
-       </div>
-     </div>
-     <!--写一个点击按钮确定和取消的按钮 -->
-     <div>
-       <div class="form-group pull-right">
-            <div>
-              <button type="button" class="btn btn-normal btn-info" @click="close()">取消</button>
-              <button type="button" class="btn btn-normal btn-info" @click="createPool()" >创建</button>
+                </tbody>
+              </table>
             </div>
+          </div>
+          <!--写一个点击按钮确定和取消的按钮 -->
+          <div>
+            <div class="form-group pull-right">
+              <div>
+                <button type="button" class="btn btn-normal btn-info" @click="close()">取消</button>
+                <button type="button" v-bind:disabled="formstate.$invalid" class="btn btn-normal btn-info" @click="createPool()" >创建</button>
+              </div>
 
-       </div>
-     </div>
-   </form>
-    </novalidator>
-
-  </div>
+            </div>
+          </div>
+      </vue-form>
     </div>
+  </div>
+
 
 </template>
 <style scoped>
+.validate{
+   display:block;
+}
+
+.validate label{
+  display:inline-block;
+  vertiacl-align:middle;
+  padding: 0 10px;
+}
 .maindiv {
-    position:fixed;
-    top:60px;
-    left:180px;
-    overflow:auto;
-    height:100%;
-    right:0;
+   position:absolute;
+  left:180px;
+  top:60px;
+  right:0px;
+  bottom:0;
+  overflow:auto;
+  /* add by 王圣文*/
+  background:#F7F7F7;
+  color:#888;
+  font: 12px Georgia, "Times New Roman", Times, serif;
+}
+.maindiv h1{
+   font-size: 25px;
+  padding: 0px 0px 10px 40px;
+  display: block;
+  border-bottom:1px solid #E4E4E4;
+  color: #888;
+}
+.alert {
+   margin-top:-10px;
+   font-size:10px;
+   margin-left:90px;
+   color:red;
+}
+.maindiv label>span {
+    float: left;
+   width: 20%;
+  text-align: right;
+  color: #888;
 }
 .div_table_left {
    display:inline-block;float:left;
@@ -165,7 +201,8 @@ export default {
         entrycheck:false,
         check_none:true,
         name:'',
-        controller:''
+        controller:'',
+        formstate:{}
       }
    },
    created: function(){
