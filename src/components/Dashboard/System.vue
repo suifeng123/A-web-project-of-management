26e57511c96cabf5df8b3eaf5c6b88a22fd9d9af
wangshengwen <wswen1220@163.com>
2017-03-22 14:30:47 +0800

    webmanagement

--- src/components/Dashboard/System.vue
+++ src/components/Dashboard/System.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="maindiv">
        <h3>这里是展示存储器整体的情况的图像</h3>
+       <h3>这里是展示存储器整体的情况的图像</h3>
+       <h3>这里是展示存储器整体的情况的图像</h3>
+       <h3>这里是展示存储器整体的情况的图像</h3>
     </div>
 </template>
 <style scoped>
