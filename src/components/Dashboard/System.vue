89de4311e82b8b31eae58f98fd21a609a31e43c8
wangshengwen <wswen1220@163.com>
2017-03-28 17:17:17 +0800

    webmanament

--- src/components/Dashboard/System.vue
+++ src/components/Dashboard/System.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="maindiv" class="maindiv">
+      {{version}}
     </div>
 </template>
 <style scoped>
@@ -10,18 +11,99 @@
     right:500px;
     bottom:300px;
 }
+.bar text {
+   font-weight:bold;
+   font-size:30px;
+}
+.bar react {
+    fill: #95859c;
+    shape-rendering: crispEdges;
+}
+.axis path,.axis line {
+  fill:none;
+  stroke:#0000;
+  shape-rendering:crispEdges;
+}
 </style>
 <script>
 import * as d3 from 'd3'
 export default {
   data() {
     return {
-      data:[1,2,3,4,5]
+      data:[1,2,3,4,5],
+      version: d3.version,
+      values: [0,1,2,3,4,5,6,7,8,9,2,3,4,5,5,5,5,5,1,1,7,7,7,6,4,8],
+      width: 960,
+      height: 500,
+      padding: 6
     }
   },
   created: function() {
-       var a = [0,10,30];
-       console.log(d3.quantile(a,2));
+       var x = d3.scale.linear().domain([0, 10]).range([0, this.width/2]);
+
+      var data = d3.layout.histogram().bins(x.ticks(10))(this.values);
+
+      var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+     var y = d3.scale.linear().domain([0, d3.max(data, function(d) { return d.x; })])
+     .range([this.height, 0]);
+
+     var yAxis = d3.svg.axis().scale(y).orient("left");
+
+     var svg = d3.select("body").append("svg")
+    .attr("width", this.width+30)
+    .attr("height", this.height+30)
+   .append("g")
+    .attr("transform", "translate(" + this.padding*2 + "," + this.padding + ")");
+
+    var bar = svg.selectAll(".bar")
+    .data(data)
+   .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+    bar.append("rect")
+ 	.attr("x",this.padding)
+    .attr("width", x(data[0].dx)-1)
+    .attr("height", function(d) { return this.height - y(d.y); });
+
+
+
+    bar.append("text")
+    .attr("dy", ".75em")
+    .attr("y", 6)
+    .attr("x", x(data[0].dx) / 2)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d.y; })
+    .attr("fill", "#fff");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate("+this.padding+"," + this.height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate("+this.padding+"," + 0 + ")")
+    .call(yAxis);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   /*
       var svg = d3.select("#maindiv")
