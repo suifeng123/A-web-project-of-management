f00402e0f72d943dd16a0b5b4beba1b49bc32b98
wangshengwen <wswen1220@163.com>
2017-05-09 15:00:36 +0800

    webmanagement

--- src/components/Dashboard/BandWidth.vue
+++ src/components/Dashboard/BandWidth.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="maindiv">
-        <h3>这是监控带宽的表格</h3>
-        <h3>helloworld</h3>
+      <div id="bindwidth"></div>
     </div>
 </template>
 <style scoped>
@@ -12,7 +11,92 @@
     left:500px;
     top:300px;
   }
+  #bindwidth {
+     height: 400px;
+     width: 400px;
+  }
 </style>
 <script>
+//引入相应的echarts控件
+import echarts from 'echarts'
 
+export default {
+  data() {
+     return {
+       //初始化对象
+       echart: null,
+       opinion: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],
+       opinionData: [
+        { value: 335, name:'直接访问'},
+        { value: 310, name:'邮件营销'},
+        { value: 234, name:'联盟广告'},
+        { value: 1548, name:'搜索引擎'},
+       ]
+     }
+  },
+  methods: {
+    drawGraph(id){
+     //开始绘制图像
+     this.echart = echarts.init(document.getElementById(id))
+     //显示加载动画
+     this.echart.showLoading()
+     var that = this;
+     //向后台发送ajax请求
+     $.ajax({
+        type:"GET",
+        async: true,
+        url: "",
+        dataType: "json",
+        success: function(result){
+           that.opinionData = result
+        },
+        error: function(errorMsg){
+           console.log(errorMsg);
+        }
+      })
+
+      this.echart.setOption({
+        title : {
+        text: '某站点用户访问来源',
+        subtext: '纯属虚构',
+        x:'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: this.opinion
+    },
+    series : [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius : '55%',
+            center: ['50%', '60%'],
+            data: this.opinionData,
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+
+      })
+
+      this.echart.hideLoading()
+
+  }
+  },
+  mounted() {
+    this.$nextTick(function() {
+      this.drawGraph('bindwidth')
+    })
+  }
+}
 </script>
