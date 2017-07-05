d291590f8f5ea38edd69d972fb82904aed8deb7f
suifeng123 <wangshengwen31@gmail.com>
2017-07-05 16:28:44 +0800

    提交一些关于elementUI的修改

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="dashboard">
-  <SettingTable />
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <style scoped>
@@ -23,12 +40,26 @@ import SettingTable from './toolsComponent/Settingtable'
 export default {
      data () {
        return {
-         formstate: {},
-         model: {
-            name: '',
-            email: 'invalid-email'
-         }
+
+         tableData: [{
+           date: '2016-05-02',
+           name: '王小虎',
+           address: '上海市普陀区金沙江路 1518 弄'
+         }, {
+           date: '2016-05-04',
+           name: '王小虎',
+           address: '上海市普陀区金沙江路 1517 弄'
+         }, {
+           date: '2016-05-01',
+           name: '王小虎',
+           address: '上海市普陀区金沙江路 1519 弄'
+         }, {
+           date: '2016-05-03',
+           name: '王小虎',
+           address: '上海市普陀区金沙江路 1516 弄'
+         }]
        }
+
      },
 
      methods: {
--- src/components/toolsComponent/Settingtable.vue
+++ src/components/toolsComponent/Settingtable.vue
@@ -1,12 +1,35 @@
 <template>
 <div>
-
+  <table class="table">
+      <thead>
+        <tr>
+          <th v-for="i in keys">{{i}}</th>
+        </tr>
+      </thead>
+    <tbody>
+       <tr>
+         <td v-for="i in data.length">{{i.name}}</td>
+       </tr>
+    </tbody>
+  </table>
 </div>
 </template>
 
 <script>
   //这个组件使用bootstrap的样式来实现
-    export default {}
+    export default {
+       data() {
+        return {
+          data:[
+            { name:'file1',size:'1G',status:'healthy',used:'70M' },
+            { name:'file2',size:'1G',status:'healthy',used:'70M' },
+            { name:'file3',size:'1G',status:'healthy',used:'70M' },
+          ],
+          keys:['name','size','status'],
+        }
+    }
+
+    }
 </script>
 
 <style>
